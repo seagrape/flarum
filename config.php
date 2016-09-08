afe38a6859de809c7be8cb46910a2a4601dd5ae7
sihan2 <sihan2@staff.sina.com.cn>
2016-09-08 17:03:10 +0800

    init for sina sae

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => '10.79.96.53',
+    'port' => '3306',
+    'database' => 'test',
+    'username' => 'root',
+    'password' => '987654',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'bbs_',
+    'strict' => false,
+  ),
+  'url' => 'http://127.0.0.1:82',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
